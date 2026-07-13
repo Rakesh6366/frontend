97f43669eabb7d16d5b31cde4ff07225e7ef9640
Rakesh6366 <mashapogurakesh@gmail.com>
2026-07-13 23:02:06 +0530

    urlchnage

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://backend-whdj.onrender.com/',
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-whdj.onrender.com/api/madicines',
   headers: { 'Content-Type': 'application/json' },
 });
 
